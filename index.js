2125d28eb00a096867d05b11a34e5b22e6704352
Susana Solis Flores <Suzy.flores@icloud.com>
2023-08-18 13:10:08 -0400

    Looping lab

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Code your solutions in this file
+const cards = ["Jeffrey", "Luis", "Luz", "Alexis"]
+
+function writeCards(names, str) {
+  for (let i = 0; i < names.length; i++) {
+    console.log(`I bought you a present ${names[i]}, happy ${str}`); 
+  }
+  
+  return names;
+}
+
+writeCards(cards, "Thanksgiving");
