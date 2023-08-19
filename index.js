b1921eb3867e7dee40786fdf3079d30e2a089d09
Susana Solis Flores <Suzy.flores@icloud.com>
2023-08-19 10:47:15 -0400

    Looping Lab

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
-const cards = ["Jeffrey", "Luis", "Luz", "Alexis"]
+// const cards = ["Jeffrey", "Luis", "Luz", "Alexis"]
 
 function writeCards(names, str) {
+  const myMessage = []
   for (let i = 0; i < names.length; i++) {
-    console.log(`I bought you a present ${names[i]}, happy ${str}`); 
-  }
-  
-  return names;
+    myMessage.push(`Thank you, ${names[i]}, for the wonderful ${str} gift!`)
 }
-
-writeCards(cards, "Thanksgiving");
+return myMessage;
+  }
+function countDown(number) {
+    let count = number;
+    while(count >= 0){ 
+      console.log(count);
+      count -= 1
+    }}
+// writeCards(cards, "Thanksgiving");
+console.log(writeCards(["Alex", "Susana", "Rita"],"Christmas"))
+console.log(writeCards(["Romina", "Juan", "Christina"],"Graduation"))
+const myArray = ["This", "array", "has", 5, "elements"];
+console.log(countDown);
